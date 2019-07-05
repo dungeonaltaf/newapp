8e1e0492b168e287247280faed8eb0b27496b130
dungeonaltaf <altafsofficial01@gmail.com>
2019-07-05 13:54:56 +0530

    this one puts js into it

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css'; 
-/*import 'bootstrap/dist/js/bootstrap.bundle.js';
-*/
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+
 import Sound from 'react-sound';
 import soundfile from './song.mp3';
 
